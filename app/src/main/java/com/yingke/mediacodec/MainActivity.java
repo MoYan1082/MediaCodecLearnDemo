d6512a854171356ebfbc34fff84a8c97b1e34354
moyan <1>
2026-01-12 16:50:55 +0800

    修复报错

--- app/src/main/java/com/yingke/mediacodec/MainActivity.java
+++ app/src/main/java/com/yingke/mediacodec/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.yingke.mediacodec.simple.MediaMuxerActivity;
 import com.yingke.mediacodec.player.MediaCodecPlayerActivity;
 
 
